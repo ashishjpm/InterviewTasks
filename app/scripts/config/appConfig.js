84370d591c93d40c682f1d598baef30a438e9b8f
ashishjpm <ashishjpm@gmail.com>
2016-08-06 18:50:40 +0530

    basic version done

--- app/scripts/config/appConfig.js
+++ app/scripts/config/appConfig.js
@@ -10,4 +10,72 @@
  */
 angular.module('interviewTasks.services')
   .constant('Settings', {
+    'jsonData':[
+        {
+            'Name': 'Ashish',
+            'Age':10,
+            'Income':20,
+            'Profit':30
+        },
+        {
+            'Name': 'Vikas',
+            'Age':40,
+            'Income':50,
+            'Profit':60
+        },
+        {
+            'Name': 'Pranav',
+            'Age':70,
+            'Income':80,
+            'Profit':90
+        },
+        {
+            'Name': 'Anubhav',
+            'Age':70,
+            'Income':80,
+            'Profit':90
+        },
+        {
+            'Name': 'Utkarsh',
+            'Age':96,
+            'Income':84,
+            'Profit':62
+        },
+        {
+            'Name': 'Asim',
+            'Age':51,
+            'Income':23,
+            'Profit':78
+        },
+        {
+            'Name': 'Santosh',
+            'Age':23,
+            'Income':45,
+            'Profit':56
+        },
+        {
+            'Name': 'Ritesh',
+            'Age':84,
+            'Income':80,
+            'Profit':26
+        },
+        {
+            'Name': 'Prakhar',
+            'Age':65,
+            'Income':34,
+            'Profit':34
+        },,
+        {
+            'Name': 'Amit',
+            'Age':52,
+            'Income':12,
+            'Profit':78
+        },,
+        {
+            'Name': 'Anuj',
+            'Age':89,
+            'Income':34,
+            'Profit':63
+        },
+    ]
   });
--- app/scripts/controllers/main.ctrl.js
+++ app/scripts/controllers/main.ctrl.js
@@ -7,13 +7,76 @@
  * Controller of the interviewTasks
  */
 angular.module('interviewTasks.controllers')
-.controller('IndexCtrl', ['$scope', '$timeout', '$state', function ($scope, $timeout, $state) {
+.controller('IndexCtrl', ['$scope', 'Settings', function ($scope, Settings) {
     //Global variables
 
     function initCtrl() {
         //Init scope variables
+        $scope.jsonData = Settings.jsonData;
+        $scope.headers = getTableHeader();
+        $scope.sortType     = 'Name'; 
+        $scope.sortReverse  = false;
+        $scope.searchTable   = '';
+        $scope.condApplied = [];
+        $scope.colors = [
+            {
+                name:'Red',
+                hex:'#f44336'
+            },
+            {
+                name:'Green',
+                hex:'#4CAF50'
+            },
+            {
+                name:'Blue',
+                hex:'#3F51B5'
+            },
+            {
+                name:'Yellow',
+                hex:'#FFEB3B'
+            }
+        ];
     }
-
+    
+    function getTableHeader(){
+        var headerArray = [];
+        for(var i=0;i<Settings.jsonData.length;i++){
+            for(var key in Settings.jsonData[i])
+                if(headerArray.indexOf(key)==-1)
+                    headerArray.push(key);
+        }
+        return headerArray;
+    }
+    
+    $scope.changeSort = function(head){
+        $scope.sortType = head; 
+        $scope.sortReverse = !$scope.sortReverse;
+    }
+    
+    $scope.applyCondClicked = function(){
+        var currentCondition = {
+            column: $scope.conditionColumn,
+            gt: $scope.conditionGT,
+            lt: $scope.conditionLT,
+            color:  $scope.conditionColor
+        }
+        $scope.condApplied.push(currentCondition);
+    }
+    
+    $scope.getFontColor = function(column,value){
+        var color = '';
+        for(var i=0;i<$scope.condApplied.length;i++)
+            if(
+                (column == $scope.condApplied[i].column)
+                &&(value > $scope.condApplied[i].gt)
+                &&(value < $scope.condApplied[i].lt)
+            ){
+                color = $scope.condApplied[i].color.name;
+                break;
+            }
+        return color;
+    }
+    
     initCtrl();
 
 }]);
