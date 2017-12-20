89b00c8e9655f125657cbc0669d9f93444b9c4f8
ashish.mishra <ashish.mishra@gdn-commerce.com>
2017-12-20 13:11:09 +0530

    step1 model link

--- app/controllers/admin/adminContestCreate.ctrl.js
+++ app/controllers/admin/adminContestCreate.ctrl.js
@@ -14,7 +14,18 @@
 
     function init(){
         $scope.root.admin.showAddBtn = false;
-    	$scope.contestCreate.currentState = 1;
+        $scope.contestCreate.currentState = 1;
+        $scope.contestCreate.stepOne = {
+            name: "",
+            description: "",
+            startDate: "",
+            endDate: "",
+            openContest: true,
+            status: "DRAFT",
+            duration: 0,
+            type: "",
+            maxTeamSize: 0
+        };
     }
 
     $scope.contestCreate.backToList = function(){
