13a51876e139f9eb635f856382ba1ffaf8a671e1
anand <anand.kumar@gdn-commerce.com>
2017-12-20 12:13:17 +0530

    backend api integration done

--- app/app.constant.js
+++ app/app.constant.js
@@ -2,7 +2,7 @@
 (function() {
   falcon
     .constant('AppConstant', {
-    	'base': 'http://localhost:8888/',
-        'api': 'http://localhost:8888/api/v1/'
+    	'base': 'http://10.177.68.50:8888/',
+        'api': 'http://10.177.68.50:8888/api/v1/'
     });
 }());
